ec642693994bc6b4ada7efa0680ed18d066b8845
error-code <danielaisoajoseph@gmail.com>
2022-10-11 05:34:34 +0100

    Code for first 3 functions
    first function get player and computer input then
    stores it in a variable
    
    second function enables computer to make a random choice
    
    third function compare choices and return result

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("Hello World");
+// player chooses between rock, paper or scissors
+let playerSelection = prompt("Rock, Paper or Scissor?");
+// computer player randomly makes a choice between rock, paper or scissors
+let computerSelection = getComputerChoice();
+
+function getComputerChoice() {
+    let choice = ["rock", "paper", "scissor"];
+    let randomNumber = Math.floor(Math.random() * choice.length);
+    return choice[randomNumber];
+}
+console.log("computer", computerSelection);
+// compare result between player and computer
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+	console.log("it's a tie");
+    } else if (playerSelection === "rock" && computerSelection !== "paper") {
+	console.log("Rock blunts Scissor!");
+    } else if (playerSelection === "paper" && computerSelection !== "scissor") {
+	console.log("Paper covers rock!");
+    } else if (playerSelection === "scissor" && computerSelection !== "rock") {
+	console.log("Scissor cuts Paper!");
+    } else {
+	console.log("awaiting results...");
+    }
+}
+playRound(playerSelection, computerSelection);
